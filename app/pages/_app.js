52e5801ea3de8c810d6f32c6da7ee3f6b48b8864
TMN <tommymartin1234@gmail.com>
2021-04-17 09:34:50 +0200

    pwa v2

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -17,12 +17,31 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
         <title>Planf'Online</title>
         <link rel="shortcut icon" href="/favicon.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#90cdf4" />
-        <link rel="apple-touch-icon" href="/logo-96x96.png" />
-        <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
       </Head>
       <QueryClientProvider client={queryClientRef.current}>
         <MainLayout>
