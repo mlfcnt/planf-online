ec304ef0b1e71e70fd3c781b6b4a055fbda4ec0b
TMN <t.martin@adsoftware.fr>
2021-06-12 08:11:31 +0200

    footer

--- app/Layout/MainLayout.jsx
+++ app/Layout/MainLayout.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Navigation } from './Navigation';
 import { useMediaQuery } from 'react-responsive'
+import { Footer } from 'antd/lib/layout/layout';
 
 
 export const MainLayout = ({ children }) => {
   const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 })
 
   return (
-    <div className="mainContainer" style={{maxWidth : isDesktopOrLaptop ? '70%' : '100%'}}>
+    <>
+    <div className="mainContainer" style={{ maxWidth: isDesktopOrLaptop ? '70%' : '100%' }}>
       <h1 className="mainTitle">Planf'Online</h1>
       <Navigation />
-      <main style={{padding : isDesktopOrLaptop ? '1.5rem' : '0rem'}}>{children}</main>
+      <main style={{ padding: isDesktopOrLaptop ? '1.5rem' : '0rem' }}>{children}</main>
     </div>
+      <Footer className='footer'>N'hésitez pas à m'envoyer vos suggestions @ tommymartin1234@gmail.com</Footer>
+      </>
   );
 }
