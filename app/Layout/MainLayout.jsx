a461bf802addcb976f6ab5670be3b9601f8e5270
TMN <t.martin@adsoftware.fr>
2021-06-15 09:05:30 +0200

    click on title to go to home

--- app/Layout/MainLayout.jsx
+++ app/Layout/MainLayout.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React from 'react';
 import { Navigation } from './Navigation';
-import { useMediaQuery } from 'react-responsive'
+import { useMediaQuery } from 'react-responsive';
 import { Footer } from 'antd/lib/layout/layout';
-
+import { useRouter } from 'next/router';
 
 export const MainLayout = ({ children }) => {
-  const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 })
+  const router = useRouter();
+  const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 });
 
   return (
     <>
       <div className="mainContainer" style={{ maxWidth: isDesktopOrLaptop ? '70%' : '100%' }}>
-        <h1 className="mainTitle">Planf'Online</h1>
+        <h1
+          className="mainTitle"
+          onClick={() =>
+            router.push({
+              pathname: '/',
+            })
+          }
+        >
+          Planf'Online
+        </h1>
         <Navigation />
         <main style={{ padding: isDesktopOrLaptop ? '1.5rem' : '0rem' }}>{children}</main>
       </div>
@@ -20,4 +30,4 @@ export const MainLayout = ({ children }) => {
       </Footer>
     </>
   );
-}
+};
